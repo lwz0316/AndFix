2588da24bfdc682ac60aa2e4538addfaca743973
lwz0316 <lwz0316@gmail.com>
2016-03-30 19:30:30 +0800

    修改 jni 配置，编译生成多个 so 文件

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,11 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
-APP_ABI := armeabi x86 
+APP_ABI := \
+        armeabi \
+        x86 \
+        arm64-v8a \
+        armeabi-v7a \
+        mips \
+        x86
+
 APP_PLATFORM := android-9
 APP_STL := gnustl_static
